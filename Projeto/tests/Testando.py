562191121a80518377620cb19abf87d8200c716b
fabianabezerra <fabiana.bezerra@icomp.ufam.edu.br>
2021-05-30 15:28:23 -0400

    Subindo projeto exemplo

---
+++ Projeto/tests/Testando.py
@@ -0,0 +1,23 @@
+import unittest
+from celsiusTofarenheit import CelsiueToFarenheit
+
+class Testando(unittest.TestCase):
+
+    def test_successo(self):
+        celsius = 41.0
+        fahrenheitEsperado = 105.8
+
+        c = CelsiueToFarenheit()
+        fahrenheitResultado = c.convert(celsius)
+        print("aqui")
+        print(fahrenheitResultado)
+        self.assertEqual(fahrenheitEsperado, fahrenheitResultado)
+
+    def test_conversao_errada(self):
+        celsius = 41.0
+        fahrenheitEsperado = 105.8
+        fahrenheitResultado = 106
+
+        self.assertNotEqual(fahrenheitResultado, fahrenheitEsperado)
+
+
---
+++ Projeto/tests/celsiusTofarenheit.py
@@ -0,0 +1,9 @@
+class CelsiueToFarenheit:
+
+    def __init__(self):
+        pass
+
+    def convert(self, celsius):
+        fahrenheit = (celsius * 1.8) + 32
+        return fahrenheit
+
